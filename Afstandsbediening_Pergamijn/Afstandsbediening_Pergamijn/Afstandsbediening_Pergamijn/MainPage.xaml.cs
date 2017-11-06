9a0f5f7fabe4d3329e8d460fc9cecdfa1a2ce906
Luca-PC\Luca <lucasmitsmans@hotmail.com>
2017-11-06 15:26:18 +0100

    Eerste opzet afstandsbediening.

---
+++ Afstandsbediening_Pergamijn/Afstandsbediening_Pergamijn/Afstandsbediening_Pergamijn/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Afstandsbediening_Pergamijn
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            Button buttonNetflix = Content.FindByName<Button>("NetflixButton");
+
+            buttonNetflix.Clicked += Button_ClickedNetflix;
+
+            Button buttonYoutube = Content.FindByName<Button>("YoutubeButton");
+
+            buttonYoutube.Clicked += Button_ClickedYoutube;
+        }
+
+        private async void Button_ClickedNetflix(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage());
+        }
+    }
+	}
+
